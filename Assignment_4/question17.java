96dcaaf716ee3f5e821195585f1b8b6b142758af
vivekmali123 <88703758+vivekmali123@users.noreply.github.com>
2022-09-05 00:31:29 +0530

    Add files via upload
    This is 4th assignment, ARRAYS.
    Main Class contains first 6 questions
    class question7 contains 7th and 8th question.
    class question9 contains 9th, 10th and 11th question.
    class question12 contains 12th, 13th, and 14th question.
    class question15 contains 15th and 16th questions.
    class question17 contains 17th question.
    class question18 contains 18th question.
    class question19 contains 19th question.

---
+++ Assignment_4/question17.java
@@ -0,0 +1,47 @@
+package Assignment_4;
+
+import java.util.Arrays;
+
+public class question17 {
+
+//Q.17. Write a method to verify if the array contains two specified elements(12,23)......
+
+	private static void checknum1(int[] arr, int toCheckValue1) {    //Check first number 12
+		boolean test = false;
+		for (int element : arr) {
+			if (element == toCheckValue1) {
+				test = true;
+				break;
+			}
+		}
+		System.out.println("Is " + toCheckValue1 + " present in the array: " + test);
+	}
+
+	private static void checknum2(int[] arr, int toCheckValue2) {    //Check first number 23
+		boolean test = false;
+		for (int element : arr) {
+			if (element == toCheckValue2) {
+				test = true;
+				break;
+			}
+		}
+		System.out.println("Is " + toCheckValue2 + " present in the array: " + test);
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println("Answer for 17th question....\n");
+		int arr[] = { 5, 1, 1, 9, 12, 23, 6, 10 };
+
+		int toCheckValue1 = 12;
+		int toCheckValue2 = 23;
+
+		System.out.println("Array: " + Arrays.toString(arr));
+		checknum1(arr, toCheckValue1);
+
+		System.out.println("Array: " + Arrays.toString(arr));
+		checknum2(arr, toCheckValue2);
+
+	}
+
+}
